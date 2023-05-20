40d12f973bf9e1abbe5b7db89dbe961c7b4046b8
hypertensiune <81163129+hypertensiune@users.noreply.github.com>
2023-05-20 18:20:04 +0300

    Add files via upload
    <bluetooth_name> settings key fix

--- client/app/src/main/java/com/example/mousedroid/MainActivity.kt
+++ client/app/src/main/java/com/example/mousedroid/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.mousedroid
 
+import android.Manifest
 import android.app.Activity
+import android.bluetooth.BluetoothManager
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -14,6 +17,7 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
 import com.google.android.material.button.MaterialButton
 import org.w3c.dom.Text
 
@@ -23,16 +27,29 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var inputActivityIntent: Intent
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        Log.d(TAG, "PERMISSION RESULT")
+        if(requestCode == 100) {
+            if(!grantResults.contains(PackageManager.PERMISSION_DENIED)) {
+                init((baseContext.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager).adapter.name)
+            }
+            else {
+                init("PERMISSION NOT ALLOWED")
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
 
+    private fun init(deviceName: String) {
         val manufacturer = Build.MANUFACTURER
         val model = Build.MODEL
-        val deviceName = Settings.Secure.getString(contentResolver, "bluetooth_name")
 
         val connectionMode = getConnectionMode()
-        TcpClient.init(manufacturer, model, deviceName, connectionMode)
+        TcpClient.init(manufacturer, deviceName, model, connectionMode)
 
         TcpClient.addOnConnectionEstablishedListener {
             inputActivityIntent = Intent(this, InputActivity::class.java)
@@ -86,6 +103,25 @@ class MainActivity : AppCompatActivity() {
             findViewById<RelativeLayout>(R.id.loadingPanel).visibility = View.GONE
             resultLauncher.launch(Intent(this, SelectDeviceActivity::class.java))
         }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S_V2) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                Log.d(TAG, "REQUESTING PERMISSION")
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.BLUETOOTH_CONNECT), 100)
+            }
+            else {
+                init((baseContext.getSystemService(BLUETOOTH_SERVICE) as BluetoothManager).adapter.name)
+            }
+        } else {
+            init(Settings.Secure.getString(contentResolver, "bluetooth_name"))
+        }
+
+        //val deviceName = Settings.Global.getString(application.contentResolver, Settings.Global.DEVICE_NAME)
     }
 
     private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
